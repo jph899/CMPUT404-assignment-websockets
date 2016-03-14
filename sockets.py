c0e0db1049b69cec3346f85614ba49b981385661
jph899 <jph@ualberta.ca>
2016-03-13 19:00:06 -0700

    Done assignment

--- sockets.py
+++ sockets.py
@@ -59,30 +59,70 @@ class World:
     def world(self):
         return self.space
 
-myWorld = World()        
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+        
+    def put(self, message):
+        self.queue.put_nowait(message)
+        
+    def get(self):
+        return self.queue.get()   
+
+myWorld = World()
+clients = list()   
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    content = {}
+    content[entity] = data
+    for client in clients:
+        client.put(json.dumps(content))
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            message = ws.receive()
+
+            if message: # something in socket
+                data = json.loads(message)
+                for i in data:
+                    myWorld.set(i, data[i])
+            else: # nothing in socket
+                break
+    except:
+        '''Received'''
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
 
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+
+    try:
+        while True:
+            message = client.get()
+            ws.send(message)
+    
+    except Exception as e:
+        print "Websocket Error %s" % e
+    
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 def flask_post_json():
     '''Ah the joys of frameworks! They do so much work for you
@@ -97,25 +137,28 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    # print data
+    for i in data:
+        myWorld.update(entity, i, data[i])
+
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
-
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
-
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
